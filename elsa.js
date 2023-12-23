c70f50322cea3af2e8fbba44fcded92f0e098bb4
elsasil <elsa97silvennoinen@gmail.com>
2023-12-23 15:10:16 +0100

    visitor information

--- elsa.js
+++ elsa.js
@@ -1,9 +1,23 @@
 
+// name form
 
-const form = document.getElementById('form');
-const submes = document.getElementById("submes")
+const nameForm = document.getElementById('nameForm');
+const nameVal = document.getElementById("nameVal")
+const name = document.getElementById("name")
+const fname = document.getElementById("fname")
+const email = document.getElementById("email")
 
-form.addEventListener("submit", (event) =>{
+
+nameForm.addEventListener("submit", (event) =>{
+    
     event.preventDefault()
-    submes.textContent = "Your name has been submitted"}
-    )
+
+    if (email.value.includes("@" && ".") === false) {
+        nameVal.textContent = "Invalid information"
+    
+    } else {nameVal.textContent = "Your infromation has been submitted"}
+    
+    })
+
+
+// något annat
