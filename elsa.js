dc8c31320d751f204085ee893ed3b89d2b1c4843
elsasil <elsa97silvennoinen@gmail.com>
2023-12-23 16:34:12 +0100

    visitor form with validation information

--- elsa.js
+++ elsa.js
@@ -3,8 +3,8 @@
 
 const nameForm = document.getElementById('nameForm');
 const nameVal = document.getElementById("nameVal")
-const name = document.getElementById("name")
 const fname = document.getElementById("fname")
+const lname = document.getElementById("lname")
 const email = document.getElementById("email")
 
 
@@ -12,12 +12,15 @@ nameForm.addEventListener("submit", (event) =>{
     
     event.preventDefault()
 
-    if (email.value.includes("@" && ".") === false) {
-        nameVal.textContent = "Invalid information"
-    
-    } else {nameVal.textContent = "Your infromation has been submitted"}
+    let letters = /^[A-Za-z]+$/
+
+    if ((email.value.includes("@" && ".") === true) && (fname.value.match(letters)) && (lname.value.match(letters)) ){
+        nameVal.textContent = "Your infromation has been submitted"
+    } else {nameVal.textContent = "Invalid information"}
     
     })
 
 
 // något annat
+
+
