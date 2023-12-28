85771d595086a4f8a9d1aa84c06978b7db2708c1
elsasil <elsa97silvennoinen@gmail.com>
2023-12-28 14:56:51 +0100

    Counting scores (JS)

--- elsa.js
+++ elsa.js
@@ -23,6 +23,39 @@ nameForm.addEventListener("submit", (event) =>{
     })
 
 
+
 // QUIZ 
 
+const quiz = document.getElementById("quiz")
+const quizVal= document.getElementById("quizVal")
+
+
+quiz.addEventListener("submit", (event) =>{
+    
+    event.preventDefault()
+
+    const answerQ1 = document.querySelector('input[name="q1"]:checked')
+    const answerQ2 = document.querySelectorAll('input[name="q2"]:checked')
+    const answerQ3 = document.getElementById("q3")
+    const answerQ4 = document.querySelector('input[name="q4"]:checked')
+    const answerQ5 = document.querySelector('input[name="q5"]:checked')
+
+    // counting score 
+    let score = 0
+    if (answerQ1 && answerQ1.value === "godric" ){score++}
+    if (answerQ2 && answerQ2.length === 3){score++}
+    if (answerQ3 && answerQ3.value.includes("james") === true){score++}
+    if (answerQ3 && answerQ3.value.includes("James") === true){score++}
+    if (answerQ4 && answerQ4.value === "otter" ){score++}
+    if (answerQ5 && answerQ5.value === "six" ){score++}
+    console.log("antal rätt", score)
+
+    // valdiation 
+    if (answerQ1){quizVal.textContent = "Your Quiz has been submitted"
+    } else {quizVal.textContent="q1 är inte besvarad"}
+
+    
+
+
+})
 
